41efb790ca1d760eae00989617638c6a40f62b26
Alaa <Alaa@DESKTOP-RC5LSOG>
2026-02-11 17:08:26 +0300

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Please enter the invoice amount:");
+
+double price = Convert.ToDouble(Console.ReadLine());
+double Priceafterdiscount;
+
+if (price >= 500)
+{
+    // 20% discount
+    Priceafterdiscount = price - (price * 0.20);
+}
+else if (price >= 300 && price <= 499)
+{
+    // 10% discount
+    Priceafterdiscount = price - (price * 0.10);
+}
+else
+{
+    // No discount
+    Priceafterdiscount = price;
+}
+
+Console.WriteLine("Final price after discount: " + Priceafterdiscount);
+
+
